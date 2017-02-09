d4665ed8dc94ea38a2f56e34a1f8de99c85e6bd8
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-02-09 13:19:06 +0100

    useless parts removed

--- src/server.js
+++ src/server.js
@@ -26,28 +26,3 @@ io.on('connection', function (socket) {
 });
 
 // http://aviadas.com/blog/2015/09/06/building-realtime-apps-with-react/
-
-
-// default react-create-app template
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
